4c755327a2ebdf28a9881409a3b71a96a32fede9
DanDev <dflopez9920@hotmail.com>
2026-01-27 18:30:05 -0500

    fix(Scaffold): Agregro librería oficial scaffold

---
+++ config/prod.exs
@@ -0,0 +1,12 @@
+import Config
+
+config :products_api,
+  timezone: "America/Bogota",
+  env: :prod,
+  http_port: 8083,
+  enable_server: true,
+  version: "0.0.1",
+  custom_metrics_prefix_name: "products_api"
+
+config :logger,
+  level: :warning
--- mix.exs
+++ mix.exs
@@ -7,23 +7,55 @@ defmodule ProductsApi.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "ca.release": :test,
+        "ca.sobelow.sonar": :test,
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.xml": :test,
+        credo: :test,
+        release: :prod,
+        sobelow: :test
+      ],
+      releases: [
+        products_api: [
+          include_executables_for: [:unix],
+          steps: [:assemble, :tar]
+        ]
+      ],
+      metrics: false
     ]
   end
 
+  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
-      mod: {ProductsApi.Application, []}
+      mod: {ProductsApi.Application, [Mix.env()]}
     ]
   end
 
+  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:castore, "~> 1.0"},
       {:plug_cowboy, "~> 2.7"},
       {:jason, "~> 1.4"},
+      {:plug_checkup, "~> 0.6"},
+      {:poison, "~> 6.0"},
+      {:cors_plug, "~> 3.0"},
+      {:timex, "~> 3.7"},
       {:uuid, "~> 1.1"},
-      {:open_api_spex, "~> 3.21"}
+      {:open_api_spex, "~> 3.21"},
+      # Test
+      {:mock, "~> 0.3", only: :test},
+      {:excoveralls, "~> 0.18", only: :test},
+      # Release
+      {:elixir_structure_manager, ">= 0.0.0", only: [:dev, :test]}
     ]
   end
 end
